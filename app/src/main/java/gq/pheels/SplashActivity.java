25a8f944399ab9a6a7d2eb99212341eec269ff3d
Vinchurkar <siddhantvinchurkar@gmail.com>
2018-08-28 00:53:57 +0530

    Splash!

--- app/src/main/java/gq/pheels/SplashActivity.java
+++ app/src/main/java/gq/pheels/SplashActivity.java
@@ -1,7 +1,11 @@
 package gq.pheels;
 
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // Wait 3 seconds before proceeding
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Moving on...", Toast.LENGTH_SHORT).show();
+            }
+        }, 3000);
+
     }
+
+    @Override
+    protected void onResume() {
+
+        /* The following code will enable immersive mode for the splash screen
+         * for devices running on Android 3.0 Honeycomb or higher. This will effectively
+         * enable immersive mode for all of the app's instances as the app is only compatible
+         * with devices running on Android 6.0 Marshmallow or higher */
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.HONEYCOMB) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+        super.onResume();
+    }
+
 }
